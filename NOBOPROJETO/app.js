3e88b5bfa3b3bbfbc91c8ced370f1a60345d623a
Brunoooo <brunotenorio15@hotmail.com>
2023-03-04 18:13:54 -0300

    servidor

--- NOBOPROJETO/app.js
+++ NOBOPROJETO/app.js
@@ -4,9 +4,7 @@ const observer = new IntersectionObserver((entries) => {
         console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
+        } 
     });
 });
 
